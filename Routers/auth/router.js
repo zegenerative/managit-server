7667b0ea4d723f23fcc85a5effbd634073f9dd22
zegenerative <zegerdevos@gmail.com>
2019-12-07 15:57:26 +0100

    Make scope string

--- Routers/auth/router.js
+++ Routers/auth/router.js
@@ -7,6 +7,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 const clientID = '7b76b39a4660b1faa24d'
 const clientSecret = process.env.REACT_APP_SECRET
+const scope = 'public_repo'
 
 const router = new Router()
 
@@ -15,7 +16,7 @@ router.get('/home', (req, res) => {
     const requestToken = req.query.code
     
     request
-        .post(`https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}&scope=user,public_repo`)
+        .post(`https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&scope=${scope}&code=${requestToken}`)
         .then(response => {
             const accessToken = response.body.access_token
             res.redirect(`${url}/home?access_token=${accessToken}`)
