ac4c9d9b81d8b51fac1bbea74b7fdb42e8852ad0
zegenerative <zegerdevos@gmail.com>
2019-12-07 16:22:57 +0100

    Change scope value

--- Routers/auth/router.js
+++ Routers/auth/router.js
@@ -7,14 +7,14 @@ if (process.env.NODE_ENV !== 'production') {
 
 const clientID = '7b76b39a4660b1faa24d'
 const clientSecret = process.env.REACT_APP_SECRET
-const scope = 'public_repo'
+const scope = 'repo'
 
 const router = new Router()
 
 router.get('/home', (req, res) => {
 
     const requestToken = req.query.code
-    
+
     request
         .post(`https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&scope=${scope}&code=${requestToken}`)
         .then(response => {
