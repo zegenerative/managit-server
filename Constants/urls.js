b78ad750843755333d10b0c5a3d0b62c14f1c1d9
zegenerative <zegerdevos@gmail.com>
2019-12-07 11:45:04 +0100

    Change url

--- Constants/urls.js
+++ Constants/urls.js
@@ -1,4 +1,4 @@
-// const url = 'http://localhost:3000'
-const url = 'https://managit-client.herokuapp.com'
+const url = 'http://localhost:3000'
+// const url = 'https://managit-client.herokuapp.com'
 
 module.exports = url
