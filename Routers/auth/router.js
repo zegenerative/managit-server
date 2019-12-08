fc3e4c9315c8bd8f0465f461cb91c9e944b0252d
zegenerative <zegerdevos@gmail.com>
2019-12-08 11:54:20 +0100

    Change url

--- Routers/auth/router.js
+++ Routers/auth/router.js
@@ -7,16 +7,16 @@ if (process.env.NODE_ENV !== 'production') {
 
 const clientID = '7b76b39a4660b1faa24d'
 const clientSecret = process.env.REACT_APP_SECRET
-const scope = 'repo'
 
 const router = new Router()
 
 router.get('/home', (req, res) => {
 
     const requestToken = req.query.code
+    const scope = 'repo'
 
     request
-        .post(`https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&scope=${scope}&code=${requestToken}`)
+        .post(`https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}&scope=${scope}`)
         .then(response => {
             const accessToken = response.body.access_token
             res.redirect(`${url}/home?access_token=${accessToken}`)
